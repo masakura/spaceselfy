9c180357d7578c850b2f3c28d094fcef333e9bda
Tomo Masakura <tomo.masakura@gmail.com>
2015-04-11 22:45:49 +0900

    地図が出た!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -64,4 +64,12 @@ $(document).ready(function () {
       finding = true;
     }
   });
+
+  var mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas"),
+                                mapOptions);
 });
